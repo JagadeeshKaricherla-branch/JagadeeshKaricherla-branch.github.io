fb08eb07809067bbb379202a2a8ed6cad35c5748
Jagadeesh Branch <102190347+JagadeeshKaricherla-branch@users.noreply.github.com>
2023-05-03 11:54:04 -0700

    Update HelloWorldApp.java

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -7,7 +7,6 @@ public class SystemDemo {
    public static void main(String[] args) throws Exception {
 
       // gets the value of the specified environment variable "SOME_SECRET"
-      System.out.println("System.getenv("SOME_SECRET") = ");
       System.out.println(System.getenv("SOME_SECRET"));
    }
 } 
