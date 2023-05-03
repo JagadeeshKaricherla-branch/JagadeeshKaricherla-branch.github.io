bce04135c46a96dd47685045dead102ba3ccdc84
Jagadeesh Branch <102190347+JagadeeshKaricherla-branch@users.noreply.github.com>
2023-05-03 12:28:18 -0700

    Update HelloWorldApp.java

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -6,7 +6,11 @@ public class SystemDemo {
 
    public static void main(String[] args) throws Exception {
 
-      // gets the value of the specified environment variable "SOME_SECRET"
-      System.out.println(System.getenv("SOME_SECRET"));
+      // gets the value of the environment variable "SOME_SECRET"
+      String read_string = System.getenv("SOME_SECRET");
+       System.out.println("Test read as secret");
+       System.out.println(System.getenv("SOME_SECRET"));
+       System.out.println("Test read as string");
+       System.out.println(read_string);
    }
 } 
