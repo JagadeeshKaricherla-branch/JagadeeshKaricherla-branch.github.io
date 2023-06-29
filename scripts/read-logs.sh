183337d299cb3902602ed3cbf6c51bb2de40be2e
Jagadeesh Branch <102190347+JagadeeshKaricherla-branch@users.noreply.github.com>
2023-06-28 21:18:17 -0700

    Update read-logs.sh (#23)
    Changelog:
    -ci(gha): fetch command added

--- scripts/read-logs.sh
+++ scripts/read-logs.sh
@@ -16,7 +16,14 @@ NC='\033[0m'
 # Set the value you want to assign to the environment variable
 my_variable="Hello, GitHub Actions!"
 
+
+# fetch tags, to be sure we have all the require info
+git fetch --tags
+
+# collect the commits since the last tag
+export GIT_RELEASE_NOTES="$(git log $(git describe --tags --abbrev=0)..HEAD --pretty=format:"%h %s")"
+
 # Write the value to the environment variable
-echo "MY_VARIABLE=$my_variable" >> $GITHUB_ENV
+echo "MY_VARIABLE=$GIT_RELEASE_NOTES" >> $GITHUB_ENV
 
 echo -en "${GREEN}Done ...${NC}\n"
