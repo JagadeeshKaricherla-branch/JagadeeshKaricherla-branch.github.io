c3a53c6951eaef19ac2db26c324a65d731c699db
Jagadeesh Branch <102190347+JagadeeshKaricherla-branch@users.noreply.github.com>
2023-06-28 21:11:26 -0700

    Update read-logs.sh

--- scripts/read-logs.sh
+++ scripts/read-logs.sh
@@ -13,5 +13,10 @@ NC='\033[0m'
 # Read
 #--------------------------------------------------------------------------------------------
 
+# Set the value you want to assign to the environment variable
+my_variable="Hello, GitHub Actions!"
+
+# Write the value to the environment variable
+echo "MY_VARIABLE=$my_variable" >> $GITHUB_ENV
 
 echo -en "${GREEN}Done ...${NC}\n"
